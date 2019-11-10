789e2b1db6cb220f6f5835372a368ee88bb8ce13
matpg <matpoga@gmail.com>
2019-11-10 03:14:16 -0300

    Anexo de index y boton v1

--- Container-LaCasa-v2/LaCasa/urls.py
+++ Container-LaCasa-v2/LaCasa/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fundacion/', include('fundacion.urls')),
+    path('', include('fundacion.urls')),
 ]
