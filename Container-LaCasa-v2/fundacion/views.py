bbac74b5ac2d5fa8390976f534ef3a090e68ed42
demf <demf@outlook.com>
2019-11-25 19:10:47 -0300

    Revert "Revert "Victor""

--- Container-LaCasa-v2/fundacion/views.py
+++ Container-LaCasa-v2/fundacion/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, get_list_or_404
-from .models import Voluntario
+from .models import Voluntario, Proyecto, Usuario
 from .forms import VoluntarioForm, SignUpForm, ProyectoForm
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate, login
 import re 
-from .funciones import CalcularEdadVoluntario
+from .funciones import CalcularEdadVoluntario, pkgen
 from datetime import datetime
 from django.contrib.auth.forms import UserCreationForm
 
@@ -23,8 +23,20 @@ class Exito_vista(TemplateView):
 
 def GestionarVoluntarios(request):
     voluntarios = get_list_or_404(Voluntario)
+    cuentas = get_list_or_404(Usuario)
     print(voluntarios)
-    return render(request, 'fundacion/gestion_voluntario.html', {'voluntarios': voluntarios})
+    return render(request, 'fundacion/gestion_voluntario.html', {'voluntarios': voluntarios}, {'cuentas': cuentas})
+
+def GestionarProyectos(request):
+    proyectos = get_list_or_404(Proyecto)
+    voluntarios = get_list_or_404(Voluntario)
+    return render(request, 'proyectos/gestion_proyectos.html', {'proyectos': proyectos}, {'voluntarios': voluntarios})
+
+def GestionarCuentas(request):
+    cuentas = get_list_or_404(Usuario)
+    proyectos = get_list_or_404(Proyecto)
+    return render(request, 'cuentas/gestion_cuentas.html', {'cuentas': cuentas}, {'proyectos': proyectos})
+
 
 ##########################Registro de Voluntarios###############################################
 
@@ -37,6 +49,7 @@ def CrearVoluntarioView(request):
             em = form_data.get("email")
             rut = form_data.get("rut")
             fecha = form_data.get("fecha")
+            proyecto_actividad = form_data.get("proyecto_actividad")
             celu = form_data.get("celular")
             comuna = form_data.get("comuna")
             obj = Voluntario()
@@ -44,17 +57,22 @@ def CrearVoluntarioView(request):
             obj.email = em
             obj.rut = rut
             obj.fecha = fecha
+            obj.proyecto_actividad = proyecto_actividad
             obj.edad = CalcularEdadVoluntario(fecha)
             obj.fecha_incripcion = datetime.now().date()
             if re.match('\d',obj.celular):
                 return render(request,'fundacion/crea_error.html', context=None)
+                print("hola")
             obj.celular = celu
             obj.comuna = comuna
             obj.save()
             return render(request,'fundacion/crea_exito.html', context=None)
-        else:
+        else:     
             return render(request,'fundacion/crea_error.html', context=None)
-    return render(request, 'fundacion/crear.html', context=None)
+        
+    proyectos = get_list_or_404(Proyecto)
+    print(Proyecto)
+    return render(request, 'fundacion/crear.html', {'proyectos': proyectos})
 
 
 ##########################Creacion de Cuentas de usuario###############################################
@@ -67,6 +85,8 @@ def register(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             #user = authenticate(username=username, password=raw_password)
+            obj= Usuario()
+            obj.save()
             return redirect("register")
 
         else:
@@ -90,8 +110,7 @@ def CreaProyecto(request):
         form = ProyectoForm(request.POST)
         if form.is_valid():
             form_data = form.cleaned_data
-            print(form_data)
-            id_proyecto = form.data.get("id_proyecto")
+            #id_proyecto = form.data.get("id_proyecto")
             nombre = form_data.get("nombre")
             descripcion = form_data.get("descripcion")
             logo = form_data.get("logo")
@@ -101,12 +120,12 @@ def CreaProyecto(request):
             cantidad_voluntarios = form_data.get("cantidad_voluntarios")
             presupuesto = form_data.get("presupuesto")
             obj = Proyecto()
-            obj.id_proyecto = id_proyecto
+            #obj.id_proyecto = id_proyecto
             obj.nombre = nombre
             obj.descricion = descripcion
             obj.logo = logo
             obj.jefe = jefe
-            obj.fecha.inicio = fecha_inicio
+            obj.fecha_inicio = fecha_inicio
             obj.fecha_termino = fecha_termino
             obj.cantidad_voluntarios = cantidad_voluntarios
             obj.presupuesto = presupuesto
