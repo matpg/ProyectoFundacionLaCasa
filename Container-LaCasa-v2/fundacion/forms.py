ec895d21f2fa80d608e263bb46dbf81057458908
demf <demf@outlook.com>
2019-11-25 18:52:41 -0300

    Revert "Victor"

--- Container-LaCasa-v2/fundacion/forms.py
+++ Container-LaCasa-v2/fundacion/forms.py
@@ -2,21 +2,19 @@ from django import forms
 import datetime 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Proyecto, Voluntario
 
 
 class VoluntarioForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     rut = forms.CharField(max_length=15)
     fecha = forms.CharField(max_length=15)
-    proyecto_actividad = forms.IntegerField()
     celular = forms.CharField(max_length=15)
     comuna = forms.CharField(max_length=50)
     email = forms.EmailField()
 
 
 class SignUpForm(UserCreationForm):
-    Nombre = forms.CharField(max_length=30, required=False, help_text='Obligatorio')
+    Nombre = forms.CharField(max_length=30, required=False, help_text='Obligatorio',)
     Apellido = forms.CharField(max_length=30, required=False, help_text='Obligatorio')
     email = forms.EmailField(max_length=254, help_text='Obligatorio')
     Usuario = forms.CharField(max_length=254, help_text='Obligatorio')
@@ -27,7 +25,7 @@ class SignUpForm(UserCreationForm):
 
 
 class ProyectoForm(forms.Form):
-    #id_proyecto = forms.CharField(max_length=6)
+    id_proyecto = forms.CharField(max_length=10)
     nombre = forms.CharField(max_length=100)
     descripcion = forms.CharField(widget=forms.Textarea)
     logo = forms.FileField(required=False)
@@ -36,5 +34,8 @@ class ProyectoForm(forms.Form):
     fecha_termino = forms.CharField(max_length=20)
     cantidad_voluntarios = forms.IntegerField()
     presupuesto = forms.CharField(max_length=20)        
+    
+    class Meta:
+        fields = ('id_proyecto', 'nombre', 'descipcion', 'logo', 'jefe', 'fecha_inicio', 'fecha_termino', 'cantidad_voluntarios','presupuesto' )
 
     
